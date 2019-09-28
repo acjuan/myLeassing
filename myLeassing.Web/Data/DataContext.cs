1bec12452cb2d6613c0e873792e909624c194eed
juan alberto <acjuan@live.com>
2019-09-28 05:07:21 -0300

    db create

---
+++ myLeassing.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myLeassing.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myLeassing.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
--- myLeassing.Web/Data/Entities/Owner.cs
+++ myLeassing.Web/Data/Entities/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,24 @@ namespace myLeassing.Web.Data.Entities
 {
     public class Owner
     {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="First Name")]
+        public string FirsName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="Last Name")]
+        public string LasName { get; set; }
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+        public string Adress { get; set; }
+        public string FullName => $"{FirsName} {LasName}";
+        public string FullNameWhitDocument => $"{FirsName} {LasName} - {Document}";
     }
 }
