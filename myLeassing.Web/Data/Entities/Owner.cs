69d8c129d6395ecf884ab475ac99e55d1e003ca9
juan alberto <acjuan@live.com>
2019-09-29 03:47:06 -0300

    drop db

--- myLeassing.Web/Data/Entities/Owner.cs
+++ myLeassing.Web/Data/Entities/Owner.cs
@@ -9,25 +9,36 @@ namespace myLeassing.Web.Data.Entities
     public class Owner
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(30)]
         public string Document { get; set; }
+
         [Required]
         [MaxLength(50)]
         [Display(Name ="First Name")]
         public string FirsName { get; set; }
+
         [Required]
         [MaxLength(50)]
         [Display(Name ="Last Name")]
         public string LasName { get; set; }
+
         [Display(Name = "Fixed Phone")]
         public string FixedPhone { get; set; }
+
         [Display(Name = "Cell Phone")]
         public string CellPhone { get; set; }
+
         public string Adress { get; set; }
+
         public string FullName => $"{FirsName} {LasName}";
+
         public string FullNameWhitDocument => $"{FirsName} {LasName} - {Document}";
+
         public ICollection<Property> Properties { get; set; }
+
         public ICollection<Contract> Contracts { get; set; }
+       
     }
 }
